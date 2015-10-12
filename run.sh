a3e84cdebb08f0505bedfe5c2bea5a779ec44926
JK-ubuntu <taikongfeizhu@163.com>
2015-10-12 23:31:19 +0800

    add init

---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+python index.py runserver -h 0.0.0.0 -p 8888 -r
---
+++ static/js/main.js
@@ -0,0 +1,6 @@
+/**
+ * Created by jenking on 15-10-7.
+ */
+$(document).ready(function(){
+    console.log("Hello World");
+});
