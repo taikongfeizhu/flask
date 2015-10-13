02dfaa0f2424a6311a8ba198a89bb97b733d2176
JK-ubuntu <taikongfeizhu@163.com>
2015-10-13 23:20:15 +0800

    modyfy index.py

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ __author__ = 'jenking'
 from flask import Flask, render_template, session, redirect, url_for, flash, make_response
 
 from flask.ext.script import Manager
+from flask.ext.script import Shell
 from flask.ext.moment import Moment
 from flask.ext.wtf import Form
 from flask.ext.bootstrap import Bootstrap
@@ -28,7 +29,6 @@ bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 manager = Manager(app)
 
-
 def trace(msg):
     now=time.strftime("%Y-%m-%d %H:%M:%S")
     print "["+now+"]",msg
@@ -36,7 +36,6 @@ def trace(msg):
 class NameForm(Form):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
-
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -57,20 +56,68 @@ class User(db.Model):
         return '<User %r>' % self.username
 
 
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+
+manager.add_command('shell', Shell(make_context=make_shell_context))
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
 
-@app.route('/data')
+
+@app.route('/data',methods=['GET','POST'])
 def database():
+    db.drop_all()
+    db.create_all()
 
+    admin_role=Role(name='Admin')
+    mod_role=Role(name='Moderator')
+    user_role=Role(name='User')
+
+    user_john=User(username='john',role=admin_role)
+    user_susan=User(username='susan',role=user_role)
+    user_david=User(username='david',role=user_role)
+
+    db.session.add(admin_role)
+    db.session.add(mod_role)
+    db.session.add(user_role)
+    db.session.add(user_john)
+    db.session.add(user_susan)
+    db.session.add(user_david)
+
+    db.session.commit()
+
+    print admin_role.id
+    print mod_role.id
+    print user_role.id
+
+    admin_role.name="Jenkingwong"
+    db.session.add(admin_role)
+    db.session.commit()
+
+    #db.session.delete(mod_role)
+    #db.session.commit()
+
+    trace( Role.query.all() )
+    trace( User.query.filter_by(role=user_role).all() )
+
+    res=user_role.users.order_by(User.username).all()
+    trace(res)
+    trace(user_role.users.count())
     return render_template('data.html')
 
 
