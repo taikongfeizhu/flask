193317ede65af291aca829a450f2a1617bdf8c70
JK-ubuntu <taikongfeizhu@163.com>
2015-10-17 14:12:00 +0800

    增加前端基础库

--- manage.py
+++ manage.py
@@ -4,11 +4,12 @@ from app import create_app, db
 from app.models import User, Role
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
+from werkzeug.contrib.fixers import ProxyFix
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
-
+app.wsgi_app=ProxyFix(app.wsgi_app)
 
 def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role)
@@ -24,4 +25,4 @@ def test():
 
 
 if __name__ == '__main__':
-    manager.run()
+    res=manager.run()
